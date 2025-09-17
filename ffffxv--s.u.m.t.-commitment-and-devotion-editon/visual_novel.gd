cac45d77c8ff72c77b7a97b24c67daf7c144ceb7
Franky Ortiz <156234855+VarsityS6@users.noreply.github.com>
2025-09-17 19:51:22 -0400

    Almost there
    Reworked the script to work with Character Portraits

--- ffffxv--s.u.m.t.-commitment-and-devotion-editon/visual_novel.gd
+++ ffffxv--s.u.m.t.-commitment-and-devotion-editon/visual_novel.gd
@@ -14,22 +14,28 @@ func load_dialogue(path: String) -> Array:
 func show_line():
 	if index >= dialogue.size():
 		$TextLabel.text = "[End of dialogue]"
+		$Portrait.texture = null
 		return
-
-
+	
 	var line = dialogue[index]
 
-	# Speaker name
 	$NameLabel.text = line.get("speaker", "")
-
-	# Dialogue text
 	$TextLabel.text = line["text"]
 
-	# Show choices if any
+	if line.has("portrait") and line["portrait"] != "":
+		var path = "res://portraits/" + line["portrait"]
+		if ResourceLoader.exists(path):
+			$Portrait.texture = load(path)
+		else:
+			$Portrait.texture = null
+	else:
+		$Portrait.texture = null 
+
 	if line.has("choices"):
 		show_choices(line["choices"])
 	else:
 		clear_choices()
+
 
 func show_choices(choices: Array):
 	clear_choices()
@@ -47,7 +53,7 @@ func clear_choices():
 		child.queue_free()
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"): # e.g. Enter/Space
+	if event.is_action_pressed("ui_accept"):
 		if not dialogue[index].has("choices"):
 			index += 1
 			show_line()
