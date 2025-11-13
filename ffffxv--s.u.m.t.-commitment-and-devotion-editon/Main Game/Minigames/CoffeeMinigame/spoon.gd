29da081ef7ab11d22e2fc9086a7a6abf1548ef3f
katattack360 <156234855+VarsityS6@users.noreply.github.com>
2025-11-13 14:28:57 -0500

    push

--- ffffxv--s.u.m.t.-commitment-and-devotion-editon/Main Game/Minigames/CoffeeMinigame/spoon.gd
+++ ffffxv--s.u.m.t.-commitment-and-devotion-editon/Main Game/Minigames/CoffeeMinigame/spoon.gd
@@ -4,7 +4,7 @@ signal stirred
 func _ready():
 	print("[Spoon] ready for clicks")
 
-func _input_event(viewport, event, shape_idx):
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MouseButton.MOUSE_BUTTON_LEFT and event.pressed:
 		print("🥄 Stirred coffee!")
 		emit_signal("stirred")
