a7269e0f91f64bfd7da9329a1485025a6909c821
Franky Ortiz <156234855+VarsityS6@users.noreply.github.com>
2026-03-12 13:13:48 -0400

    button

--- ffffxv--s.u.m.t.-commitment-and-devotion-editon/Main Menu/control.gd
+++ ffffxv--s.u.m.t.-commitment-and-devotion-editon/Main Menu/control.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main Game/Scenes/FirstScene.tscn")
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main Game/Scenes/FirstScene.tscn")
